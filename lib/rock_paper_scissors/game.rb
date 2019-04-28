ad914ff9c5181431c34ce15b0029f7780dc8c77d
Rajas Pargaonkar <rajas@waddleworks.com>
2019-04-28 15:03:52 -0400

    Add Game class and spec

--- lib/rock_paper_scissors/game.rb
+++ lib/rock_paper_scissors/game.rb
@@ -1,11 +1,125 @@
 module RockPaperScissors
   class Game
-    def initialize(player)
-      @player = player
-      @rounds = {}  
+    attr_reader :winner
+    include Comparable 
+    #include Utilities #for custom titleize
+
+    RPS = [ 'rock', 'paper', 'scissors' ]
+
+    def initialize(session, name)
+      @session = session
+      #@name = session.player.name # Design question: Should this class know that session has player and name on it?
+      @name = name
+      @record = { @name => 0, 'Computer' => 0 }
+      @winner = nil
+      @rounds = 0
+      @computer_choice = nil
+      @player_choice = nil
     end
 
     def play
+      display_countdown 
+      
+      @player_choice, @computer_choice = gets.chomp.downcase, computer_roll.downcase
+      puts
+
+      if RPS.include?(@player_choice)
+        judge_round
+
+        @winner ? show_ending : play
+      else
+       @winner = 'Computer' 
+       puts "#{@player_choice}????"
+       puts
+       puts 'That makes no sense. Congratulations on losing Rock Paper Scissors on a technicality.'
+       puts
+
+       show_ending
+      end
+    end
+
+    private
+
+    def display_countdown
+      sleep(0.5)
+      puts '1'
+      puts
+      sleep(0.5)
+      puts '2' 
+      puts
+      sleep(0.5)
+      puts '3' 
+      puts
+      puts 'SHOOT!'
+      puts
+    end
+
+    def computer_roll
+      RPS.sample
+    end
+
+    def judge_round
+      if self > @player_choice
+        puts "Computer wins!"
+        puts "#{@computer_choice} beats #{@player_choice}"
+        puts 
+        tally_score('Computer')
+      elsif self < @player_choice
+        puts "#{@name} wins!"
+        puts "#{@player_choice} beats #{@computer_choice}" 
+        puts    
+        tally_score(@name)
+      else
+        puts "#{@computer_choice} equals #{@player_choice}"
+        puts "The round is a tie"
+        puts
+        tally_score('Nobody')
+      end
+    end
+
+    def <=>(player_choice)
+      compare_choices(@player_choice)
+    end
+
+    def compare_choices(player_choice)
+      a, b = @computer_choice, @player_choice
+
+      case
+      when a == 'rock'      && b == 'scissors' then  1
+      when a == 'paper'     && b == 'rock'     then  1
+      when a == 'scissors'  && b == 'paper'    then  1
+      when a == 'rock'      && b == 'paper'    then -1
+      when a == 'paper'     && b == 'scissors' then -1
+      when a == 'scissors'  && b == 'rock'     then -1
+      when a == 'rock'      && b == 'rock'     then  0
+      when a == 'paper'     && b == 'paper'    then  0
+      when a == 'scissors'  && b == 'scissors' then  0
+      end
+    end
+
+    def tally_score(winner)
+      @record[winner] += 1 unless winner == 'Nobody'
+      @rounds += 1
+
+      check_for_winner
+    end
+
+    def check_for_winner
+      neither_player_has_any_wins = (@record[@name] == 0 && @record['Computer'] == 0)
+
+      @winner = 'Computer' if @record['Computer'] == 3  
+      @winner = @name      if @record[@name] == 3  
+      @winner = 'Nobody'   if (@rounds == 5 && neither_player_has_any_wins)    
+    end
+
+    def show_ending
+      puts "OMG! #{@winner} WINS!"
+      puts
+      exit_game
+    end
+
+    def exit_game
+      @session.end_player_session
     end
   end
 end
--- spec/rock_paper_scissors/game_spec.rb
+++ spec/rock_paper_scissors/game_spec.rb
@@ -1,5 +1,126 @@
 RSpec.describe RockPaperScissors::Game do
-  describe 'play' do
+  describe '#play' do
+    let(:name) { 'Rajas' }
+    let(:player) { RockPaperScissors::Player.new(name) }
+    let(:cli) { RockPaperScissors::CLI.new }
+    let(:game) { RockPaperScissors::Game.new(cli, name) }
+    let(:computer) { 'Computer' }
+    let(:draw) { 'Nobody' }
+    let(:player_wins_script) { File.read('spec/fixtures/player_wins_3_straight.txt') }
+    let(:computer_wins_script) { File.read('spec/fixtures/computer_wins_3_straight.txt') }
+    let(:draw_script) { File.read('spec/fixtures/draw.txt') }
+    let(:invalid_input_script) { File.read('spec/fixtures/invalid_input.txt') }
 
+    describe 'player wins 3 straight' do
+      before do
+        allow(RockPaperScissors::CLI).to receive(:new).and_return(cli)
+        cli.instance_variable_set(:@player, player) 
+        
+        allow_any_instance_of(RockPaperScissors::Game).to receive(:gets).and_return('Rock', 'Paper', 'Scissors')
+        allow_any_instance_of(RockPaperScissors::Game).to receive(:computer_roll).and_return('Scissors', 'Rock', 'Paper')
+      end
+
+      it 'displays the correct script to the player' do #TODO: This will need to add the show ending text, and titleized values for R, P, S
+        expect do        
+          game.play
+        end.to output(a_string_matching(player_wins_script)).to_stdout
+      end
+
+      it 'sets the player as the winner of the game' do
+        game.play
+        
+        expect(game.instance_variable_get('@winner')).to eq(player.name)
+      end
+
+      it 'take the player back to the cli' do
+        expect(cli).to receive(:end_player_session)
+
+        game.play
+      end
+    end
+
+    describe 'computer wins 3 straight' do
+      before do
+        allow(RockPaperScissors::CLI).to receive(:new).and_return(cli)
+        cli.instance_variable_set(:@player, player) 
+
+        allow_any_instance_of(RockPaperScissors::Game).to receive(:gets).and_return('Scissors', 'Rock', 'Paper')
+        allow_any_instance_of(RockPaperScissors::Game).to receive(:computer_roll).and_return('Rock', 'Paper', 'Scissors')
+      end
+
+      it 'displays the correct script to the player' do
+        expect do        
+          game.play
+        end.to output(a_string_including(computer_wins_script)).to_stdout
+      end
+
+      it 'sets the computer as the winner of the game' do
+        game.play
+
+        expect(game.instance_variable_get('@winner')).to eq(computer)
+      end
+
+      it 'takes the player back to the cli' do
+        expect(cli).to receive(:end_player_session)
+
+        game.play
+      end
+    end
+
+    describe 'the game ends in a draw' do
+      before do
+        allow(RockPaperScissors::CLI).to receive(:new).and_return(cli)
+        cli.instance_variable_set(:@player, player) 
+
+        allow_any_instance_of(RockPaperScissors::Game).to receive(:gets).and_return('Rock', 'Rock', 'Rock', 'Rock', 'Rock') 
+        allow_any_instance_of(RockPaperScissors::Game).to receive(:computer_roll).and_return('Rock', 'Rock', 'Rock', 'Rock', 'Rock')
+      end
+
+      it 'displays the correct script to the player' do
+        expect do
+          game.play
+        end.to output(a_string_including(draw_script)).to_stdout
+      end
+
+      it 'sets \'Nobody\' as the winner of the game' do
+        game.play
+
+        expect(game.instance_variable_get('@winner')).to eq(draw)
+      end
+
+      it 'take the player back to the cli' do
+        expect(cli).to receive(:end_player_session)
+
+        game.play
+      end
+    end
+
+    describe 'the user enters an invalid input at any point' do
+      before do
+        allow(RockPaperScissors).to receive(:new).and_return(cli)
+        cli.instance_variable_set(:@player, player)
+
+        allow_any_instance_of(RockPaperScissors::Game).to receive(:gets).and_return('nonsense') 
+        allow_any_instance_of(RockPaperScissors::Game).to receive(:computer_roll).and_return('Paper')
+      end
+
+      it 'displays the correct script to the player' do
+        expect do
+          game.play
+        end.to output(a_string_matching(invalid_input_script)).to_stdout
+      end
+
+      it 'sets \'Computer\' as the winner of the game' do
+        game.play
+
+        expect(game.instance_variable_get('@winner')).to eq(computer)
+      end
+
+      it 'take the player back to the cli' do
+        expect(cli).to receive(:end_player_session)
+
+        game.play
+      end
+    end
   end 
 end
